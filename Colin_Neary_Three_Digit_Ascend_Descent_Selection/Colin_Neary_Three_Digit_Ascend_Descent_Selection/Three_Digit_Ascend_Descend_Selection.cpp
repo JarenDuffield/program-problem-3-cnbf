2d913dc9003d89395d96a0509489c524e85d36cb
JarenDuffield <31770178+JarenDuffield@users.noreply.github.com>
2017-10-12 09:06:21 -0500

    It loops now :)
    10/12/17 
    We changed the cout and added a while loop.

--- Colin_Neary_Three_Digit_Ascend_Descent_Selection/Colin_Neary_Three_Digit_Ascend_Descent_Selection/Three_Digit_Ascend_Descend_Selection.cpp
+++ Colin_Neary_Three_Digit_Ascend_Descent_Selection/Colin_Neary_Three_Digit_Ascend_Descent_Selection/Three_Digit_Ascend_Descend_Selection.cpp
@@ -6,6 +6,8 @@ Three_Digit_Ascend_Descend_Selection : *
 
 Determining order of digits
 
+Modified by Christopher Hartsfield and Jaren Duffield
+
 */
 
 #include <iostream>
@@ -20,29 +22,36 @@ void pause() {
 	cout << '\n';
 
 }
-void main() {
+int main()
+{
+	int i = 1;
+	while (i < 30)
+	{
 	int x;
-	cout << "Press any positive three digit number";
+	cout << "Press any positive three digit number ";
 	cin >> x;
-	int a = (x/100) % 10;
+	int a = (x / 100) % 10;
 	int b = (x / 10) % 10;
 	int c = x % 10;
-	bool ascending = a!= b && b!= c;
+	bool ascending = a != b && b != c;
 	bool descending = ascending;
-	if(a > b)
-		ascending = false;
-	else
-		descending = false;
-	if(b > c)
-		ascending = false;
-	else
-		descending = false;
-	if(ascending)
-		cout << "Ascending \n";
-	else if(descending)
-		cout << "Descending \n";
-	else
-		cout << "Neither ";
+	if (a > b)
+			ascending = false;
+			else
+			descending = false;
+			if (b > c)
+				ascending = false;
+			else
+				descending = false;
+			if (ascending)
+				cout << "The number " << x << " is ascending \n";
+			else if (descending)
+				cout << "The number " << x << " is descending \n";
+			else
+				cout << "The number " << x << " is neither ascending or descending \n";
+			i++;
+			_getch();
+		}
+		return 0;
+	}
 
-	pause();
-}
